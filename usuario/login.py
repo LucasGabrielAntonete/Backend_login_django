2f8a9ba690bb718005bfedc1e4aaee7298b4022d
LucasGabrielAntonete <lucasantonete@gmail.com>
2024-03-16 16:59:34 -0300

    fix

--- usuario/login.py
+++ usuario/login.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.utils.translation import gettext_lazy as _
 from rest_framework.permissions import AllowAny
-from usuario.models import Usuario
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Usuario
