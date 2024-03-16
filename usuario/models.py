3967b527b9a863c3560e00c7b35c3757d08c86be
LucasGabrielAntonete <lucasantonete@gmail.com>
2024-03-15 22:20:15 -0300

    Criação de backend para login

---
+++ usuario/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class Usuario(AbstractUser):
+    username = None
+    email = models.EmailField(_("e-mail address"), unique=True)
+    data_nascimento = models.DateField(
+        _("Birth Date"), auto_now=False, auto_now_add=False, blank=True, null=True
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    EMAIL_FIELD = "email"
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        ordering = ["-date_joined"]
